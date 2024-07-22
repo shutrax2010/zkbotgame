714044c4bf1526c7618b902a5717089c22eb66c0
yahomi-dev <yahomi.dev@gmail.com>
2024-07-23 06:00:55 +0900

    io追加

--- apps/frontend/src/components/GamePage.tsx
+++ apps/frontend/src/components/GamePage.tsx
@@ -4,14 +4,16 @@ import Game from './Game';
 import Header from './Header';
 import SideBar from './SideBar';
 
-const GamePage: React.FC = () => (
-  <div className="flex flex-col h-screen">
-    <Header />
-    <div className="flex flex-1 overflow-hidden">
-      <SideBar />
-      <Game />
+const GamePage: React.FC = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <div className="flex flex-1 overflow-hidden">
+        <SideBar />
+        <Game />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default GamePage;
